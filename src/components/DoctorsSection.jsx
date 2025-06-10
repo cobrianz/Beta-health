868ffac5aaab9bd811a068b2afb13f5107fd58f3
cobrianz <briancheruiyot.h@gmail.com>
2025-06-10 21:56:55 +0300

    update

--- src/components/DoctorsSection.jsx
+++ src/components/DoctorsSection.jsx
@@ -1,9 +1,11 @@
+import React from "react";
 import {
   FacebookLogo,
   TwitterLogo,
   InstagramLogo,
   LinkedinLogo,
 } from "@phosphor-icons/react";
+
 import doc1 from "../assets/doctor-1.jpg";
 import doc2 from "../assets/doctor-2.jpg";
 import doc3 from "../assets/doctor-3.jpg";
@@ -13,12 +15,7 @@ const doctors = [
   { name: "Dr. Smith McDonald", title: "Cardiologist", img: doc1 },
   { name: "Dr. Derek McDonald", title: "Cardiologist", img: doc2 },
   { name: "Dr. Manuel Tannehill", title: "Orthopedic", img: doc3 },
-  {
-    name: "Dr. Virginia Erickson",
-    title: "Gastroenterologist",
-    img: doc4,
-    social: true,
-  },
+  { name: "Dr. Virginia Erickson", title: "Gastroenterologist", img: doc4 },
 ];
 
 const DoctorsSection = () => {
@@ -30,18 +27,18 @@ const DoctorsSection = () => {
         {doctors.map((doc, index) => (
           <div className="doctor-card" key={index}>
             <img src={doc.img} alt={doc.name} className="doctor-img" />
-            <div className="doctor-info">
-              <h3>{doc.name}</h3>
-              <p>{doc.title}</p>
-            </div>
-            {doc.social && (
+            <div className="content">
+              <div className="doctor-info">
+                <h3>{doc.name}</h3>
+                <p>{doc.title}</p>
+              </div>
               <div className="social-icons">
                 <FacebookLogo size={20} weight="fill" />
                 <TwitterLogo size={20} weight="fill" />
                 <LinkedinLogo size={20} weight="fill" />
                 <InstagramLogo size={20} weight="fill" />
               </div>
-            )}
+            </div>
           </div>
         ))}
       </div>

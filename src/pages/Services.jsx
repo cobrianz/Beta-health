1b5c4311fcded426140a1e196d8c1aea22dee5b4
cobrianz <briancheruiyot.h@gmail.com>
2025-06-15 22:44:52 +0300

    update

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,12 @@
 import DynamicHeader from './../components/DynamicHeader';
 import TelemedicineServices from './../components/TelemedicineServices';
+import NewsletterSignup from './../components/NewsletterSignup';
 const Services = () => {
   return (
     <div>
       <DynamicHeader />
       <TelemedicineServices />
+      <NewsletterSignup />
     </div>
   );
 };
